3cd35fd73d79f6e46223f6ebec2dcd485165d944
Lubna Naqvi <lubnanaqvi11@gmail.com>
2019-02-21 19:27:33 -0500

    a message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World, Day 1 group project kick off! Another chabng]]</h1>
+        <h1>Hello World, Day 1 group project kick off! Another change</h1>
       </div>
     );
   }
