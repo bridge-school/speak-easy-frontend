52e46b75e21895fa6526b9ae7c9a9eecf84e0a28
Heather Stephens <stephens.heather@gmail.com>
2019-03-03 22:44:43 -0500

    BrowserRouter added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Header from './components/Header';
+import Form from './components/Form';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Route path="/" component={Header} />
+        <Route path="/submit-conference" component={Form} />
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from './Button';
 
 const Header = () => {
   return (
     <header>
       Call for Diversity
-      <Button />
+      <Link to="/submit-conference">
+        <Button />
+      </Link>
     </header>
   );
 };
