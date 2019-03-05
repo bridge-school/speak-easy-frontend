3cdb6d6d7a2c18ec9b7cfef248f6c349be95d282
nsbtr <ngsabater@gmail.com>
2019-03-05 01:01:23 -0500

    Install node-sass and set up custom tachyon colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import 'tachyons';
 import Header from './components/Header';
+import './App.scss';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <button className="bg-bright-blue">Mint green</button>
       </div>
     );
   }
