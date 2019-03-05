7e9eb26f1d36eb0fc160f6167c2ddf905bff25e4
Heather Stephens <stephens.heather@gmail.com>
2019-03-05 18:57:51 -0500

    BrowserRouter, passing tests

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Header from './components/Header';
-import Form from './components/Form';
+import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Route path="/" component={Header} />
-      <Route path="/submit-conference" component={Form} />
-    </div>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
