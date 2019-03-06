5a9f90f141bde2519f46973b86961e82711e0429
Heather Stephens <stephens.heather@gmail.com>
2019-03-05 20:58:12 -0500

    Styled Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Button from './Button';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ className, title }) => {
   return (
-    <header>
-      Call for Diversity
-      <Link to="/submit-conference">
-        <Button />
-      </Link>
+    <header className="bb br1 b--lavender pa3 bg-white f4 tl serif i">
+      <div className="w-80 center mb2">
+        Call for Diversity
+        <Link to="/submit-conference">
+          <div className="fr">
+            <Button title="Submit Event" />
+          </div>
+        </Link>
+      </div>
     </header>
   );
 };
