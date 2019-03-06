664b8718ea9af32a482e1c54cc1f9f97bb032eba
hmstephens <45745153+hmstephens@users.noreply.github.com>
2019-03-05 19:01:09 -0500

    BrowserRouter added (#15)
    * BrowserRouter added
    
    * Moved Routes to BrowserRouter component
    
    * BrowserRouter, passing tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <BrowserRouter>
+          <div>
+            <Route path="/" component={Header} />
+            <Route path="/submit-conference" component={Form} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
