fe9dd82e3ffd8946b538a02409da7f2ec00808ac
Lubna Naqvi <lubnanaqvi11@gmail.com>
2019-03-13 19:39:09 -0400

    form-validation

--- src/utils.js
+++ src/utils.js
@@ -9,15 +9,19 @@ export const isFormDataValid = formData => {
       '\nAll fields are required. Please make sure that all fields are filled.';
 
   //match regex for email
-  const emailRe = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailRe = /^[\w.-]+@[\w.-]+.[a-zA-Z]+$/;
   if (!emailRe.test(formData.contactEmail))
     validateMessage +=
       '\nPlease provide a valid email address for Contact Email.';
 
-  //match regex for url
-  const urlRe = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+  //match regex for url of Event Website
+  const urlRe = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
   if (!urlRe.test(formData.eventWebsite))
     validateMessage += '\nPlease enter a valid URL for Event Website.';
+
+  //match regex for url for submission website
+  if (!urlRe.test(formData.submissionWebsite))
+    validateMessage += '\nPlease enter a valid URL for Submission Website.';
 
   //check submissionDueDate is not before today
   const enteredSubmissionDueDate = new Date(formData.submissionDueDate);
