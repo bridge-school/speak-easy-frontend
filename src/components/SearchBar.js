ea11d7923e4e4ccbe091847db301b7f5e421b819
ahellyer <39972988+ahellyer@users.noreply.github.com>
2019-03-14 19:16:45 -0400

    added in search bar component and filtering function (#22)
    * added in search bar component and filtering function in the ConferenceList component
    
    * added key to conference card

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { onSearchChange } from '../redux/actions';
+import SearchIcon from '../assets/search-icon.png';
+
+const SearchBar = ({ searchParam, onSearchChange }) => {
+  const handleChange = e => {
+    onSearchChange(e.target.value);
+  };
+  return (
+    <input
+      style={{
+        backgroundImage: `url(${SearchIcon})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '40px',
+        backgroundPositionY: '5px'
+      }}
+      className="ba b--black-20 border-box w-100 center bg-white pa3 pl5 sans-serif ma3 tl "
+      value={searchParam}
+      onChange={handleChange}
+      placeholder="Search for event by name"
+    />
+  );
+};
+
+//onChange update store
+const mapStateToProps = store => {
+  return {
+    searchParam: store.searchParam
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  onSearchChange: input => dispatch(onSearchChange(input))
+});
+
+const ConnectedSearchBar = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SearchBar);
+
+export default ConnectedSearchBar;
