9bd27bfdc3ad2e8b6c67fb6c433cd7d98108d46a
lubnanaqvi <36479076+lubnanaqvi@users.noreply.github.com>
2019-03-14 19:35:47 -0400

    Formvalidate (#25)
    * formSubmit
    
    * formvalidate
    
    * form-validation

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,15 +1,19 @@
+import { isFormDataValid } from '../utils';
+
 export const UPDATE_FORM_DATA = 'UPDATE_FORM_DATA';
 export const FORM_SUBMIT_START = 'FORM_SUBMIT_START';
 export const FORM_SUBMIT_SUCCESS = 'FORM_SUBMIT_SUCCESS';
 export const LIST_FETCH_START = 'LIST_FETCH_START';
 export const LIST_FETCH_SUCCESS = 'LIST_FETCH_SUCCESS';
 export const UPDATE_CONFERENCES = 'UPDATE_CONFERENCES';
+
 export const UPDATE_SEARCH_PARAM = 'UPDATE_SEARCH_PARAM';
 
 export const onSearchChange = input => ({
   type: UPDATE_SEARCH_PARAM,
   payload: input
 });
+
 
 export const submitFormToStore = payload => {
   return { type: UPDATE_FORM_DATA, payload };
@@ -18,10 +22,15 @@ export const submitFormToStore = payload => {
 export const handleFormSubmit = formData => {
   return dispatch => {
     dispatch(submitFormToStore(formData));
+
+    const validateMessage = isFormDataValid(formData);
+    if (validateMessage.length === 0) {
+
     const emptyFormFields = Object.keys(formData)
       .map(value => formData[value])
       .filter(x => x === '').length;
     if (emptyFormFields === 0) {
+
       fetch('http://localhost:8081/conferences', {
         method: 'POST',
         body: JSON.stringify(formData),
@@ -31,7 +40,10 @@ export const handleFormSubmit = formData => {
       })
         .then(res => console.log(res))
         .catch(err => console.log(err));
-    }
+    } else
+      alert('Form validation returned following errors:\n' + validateMessage);
   };
 };
 
+    }
+ 
