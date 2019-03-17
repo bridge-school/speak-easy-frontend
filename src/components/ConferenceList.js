7738bb8948dd6cacf726ed82b71932ed22d1dc7c
Ashley <ashleyhellyer@gmail.com>
2019-03-17 18:39:35 -0400

    added button to each ConferenceCard, on click renders contact details

--- src/components/ConferenceList.js
+++ src/components/ConferenceList.js
@@ -10,7 +10,6 @@ export class ConferenceList extends React.Component {
   }
 
   render() {
-    console.log(this.props.searchParam);
     const filteredList = this.props.conferences.filter(
       item =>
         item.eventName
