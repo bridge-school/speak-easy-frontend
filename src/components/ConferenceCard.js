fb7b2747dba1ee996b107338859a13fd008ec479
hmstephens <45745153+hmstephens@users.noreply.github.com>
2019-03-18 00:18:50 -0400

    Recaptcha (#29)
    * added button to each ConferenceCard, on click renders contact details
    
    * added recaptcha feature to submission form

--- src/components/ConferenceCard.js
+++ src/components/ConferenceCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { formatDate } from '../utils';
 import Compensation from '../assets/compensation.png';
 import Scholarship from '../assets/scholarship.png';
@@ -18,53 +18,76 @@ const BlackOrGray = ({ isActive, children, icon, alt }) => {
   );
 };
 
-const ConferenceCard = props => {
-  const {
-    eventName,
-    eventWebsite,
-    submissionDueDate,
-    eventDate,
-    location,
-    isCompensated,
-    hasCodeOfConduct,
-    hasDiversityScholarship
-  } = props.conferenceData;
-  return (
-    <div className="br3 ba b--black-20 mw5 mw7-ns center bg-white pa3  sans-serif ma3 tl">
-      <div className="w-100 cf sans-serif pv2">
-        <a href={eventWebsite}>
-          <h3 className="di tl bright-blue">{eventName}</h3>
+class ConferenceCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false
+    };
+  }
+  render() {
+    const {
+      eventName,
+      eventWebsite,
+      submissionDueDate,
+      eventDate,
+      eventLocation,
+      isCompensated,
+      hasCodeOfConduct,
+      hasDiversityScholarship,
+      contactEmail,
+      contactName
+    } = this.props.conferenceData;
+    return (
+      <div className="br3 ba b--black-20 mw5 mw7-ns center bg-white pa3  sans-serif ma3 tl">
+        <div className="w-100 cf sans-serif pv2">
+          <a href={eventWebsite}>
+            <h3 className="di tl bright-blue">{eventName}</h3>
+          </a>
+          <p className="di fr black ma0">{formatDate(submissionDueDate)}</p>
+        </div>
+        <p className="di pa0 mid-gray">{formatDate(eventDate)}</p>
+        <p className="di ph2 mid-gray">•</p>
+        <p className="di pa0 mid-gray">{eventLocation}</p>
+        <ul className=" mw10 pl0 pt3">
+          <BlackOrGray
+            isActive={isCompensated}
+            icon={Compensation}
+            alt="dollar sign"
+          >
+            Compensation
+          </BlackOrGray>
+          <BlackOrGray
+            isActive={hasCodeOfConduct}
+            icon={Conduct}
+            alt="check mark"
+          >
+            Code of Conduct
+          </BlackOrGray>
+          <BlackOrGray
+            isActive={hasDiversityScholarship}
+            icon={Scholarship}
+            alt="graduation cap"
+          >
+            Diversity Scholarship
+          </BlackOrGray>
+        </ul>
+        {this.state.isExpanded && (
+          <div>
+            <p className="mv1">Contact Email: {contactEmail}</p>
+            <p className="mv1">Contact Name: {contactName}</p>
+          </div>
+        )}
+        <a
+          className="f6 link dim ph3 pv2 mb2 dib bright-blue mv2"
+          href="#0"
+          onClick={() => this.setState({ isExpanded: !this.state.isExpanded })}
+        >
+          {this.state.isExpanded ? 'See less' : 'See more...'}
         </a>
-        <p className="di fr black ma0">{submissionDueDate}</p>
       </div>
-      <p className="di pa0 mid-gray">{formatDate(eventDate)}</p>
-      <p className="di ph2 mid-gray">•</p>
-      <p className="di pa0 mid-gray">{location}</p>
-      <ul className=" mw10 pl0 pt3">
-        <BlackOrGray
-          isActive={isCompensated}
-          icon={Compensation}
-          alt="dollar sign"
-        >
-          Compensation
-        </BlackOrGray>
-        <BlackOrGray
-          isActive={hasCodeOfConduct}
-          icon={Conduct}
-          alt="check mark"
-        >
-          Code of Conduct
-        </BlackOrGray>
-        <BlackOrGray
-          isActive={hasDiversityScholarship}
-          icon={Scholarship}
-          alt="graduation cap"
-        >
-          Diversity Scholarship
-        </BlackOrGray>
-      </ul>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ConferenceCard;
