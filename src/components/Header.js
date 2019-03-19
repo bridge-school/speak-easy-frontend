3ae9cadf4a81216570405674e30db4fc97757bd2
nsbtr <ngsabater@gmail.com>
2019-03-19 00:59:17 -0400

    Turn title in header into a link to the homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,9 @@ const Header = () => {
   return (
     <header className="bb br1 b--lavender pa3 bg-white f4 tl serif i">
       <h1 className="w-60 f3 center ma0 mb2 normal">
-        Call for Diversity
+        <Link to="/" className="black no-underline">
+          Call for Diversity
+        </Link>
         <Link to="/submit-conference">
           <div className="fr">
             <Button title="Submit Event" />
